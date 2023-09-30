6101d492ae61c4f50aadcc42f705b4f8c8fc1638
Pravargav <96449025+Pravargav@users.noreply.github.com>
2023-09-30 15:55:08 +0530

    oerogsos
    hhoogjgi

--- urls.py
+++ urls.py
@@ -3,35 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('s1/', views.subject1, name='subject1'),
-    path('s2/', views.subject2, name='subject2'),
-    path('s3/', views.subject3, name='subject3'),
-    path('s4/', views.subject4, name='subject4'),
+    path('s/', views.subject, name='subject'),
 
-    path('s1/',include([
+
+    path('s/',include([
         path('theory/', views.theory),
-        path('radio/', views.radio),
-        path('checkbox/', views.checkbox),
+
     ])),
-    path('s2/',include([
-        path('theory/', views.theory),
-        path('radio/', views.radio),
-        path('checkbox/', views.checkbox),
-    ])),
-    path('s3/',include([
-        path('theory/', views.theory),
-        path('radio/', views.radio),
-        path('checkbox/', views.checkbox),
-    ])),
-    path('s4/',include([
-        path('theory/', views.theory),
-        path('radio/', views.radio),
-        path('checkbox/', views.checkbox),
-    ])),
-    
-    path('pdf/', views.some_view, name='some_view'),
-    path('csv2/', views.some_view2, name='some_view2'),
-    path('csv3/', views.some_streaming_csv_view, name='some_streaming_csv_view'),
-   
-    
+
+    path('pdf/', views.some_view, name='some_view'), 
 ] 
--- views.py
+++ views.py
@@ -1,38 +1,18 @@
-
-from django.shortcuts import render,HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 
 def index(request):
     context={}
     return render(request, 'takeTest/index.html', context)
 
-def subject1(request):
+def subject(request):
     context={}
-    return render(request, 'takeTest/subject1.html', context)
-
-def subject2(request):
-    context={}
-    return render(request, 'takeTest/subject2.html', context)
-
-def subject3(request):
-    context={}
-    return render(request, 'takeTest/subject3.html', context)
-
-def subject4(request):
-    context={}
-    return render(request, 'takeTest/subject4.html', context)
+    return render(request, 'takeTest/subject.html', context)
 
 def theory(request):
     context={}
     return render(request, 'takeTest/theory.html', context)
 
-def radio(request):
-    context={}
-    return render(request, 'takeTest/radio.html', context)
 
-def checkbox(request):
-    context={}
-    return render(request, 'takeTest/checkbox.html', context)
 
 import io
 from django.http import FileResponse
@@ -47,38 +27,6 @@ def some_view(request):
     buffer.seek(0)
     return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
 
-import csv
-from django.http import HttpResponse
 
-def some_view2(request):
-    response = HttpResponse(
-        content_type='text/csv',
-        headers={'Content-Disposition': 'attachment; filename="somefilename.csv"'},
-    )
-
-    writer = csv.writer(response)
-    writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
-    writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
-
-    return response
-
-import csv
-from django.http import StreamingHttpResponse
-
-class Echo:
-   
-    def write(self, value):
-        return value
-
-def some_streaming_csv_view(request):
-
-    rows = (["Row {}".format(idx), str(idx)] for idx in range(65536))
-    pseudo_buffer = Echo()
-    writer = csv.writer(pseudo_buffer)
-    return StreamingHttpResponse(
-        (writer.writerow(row) for row in rows),
-        content_type="text/csv",
-        headers={'Content-Disposition': 'attachment; filename="somefilename.csv"'},
-    )
 
 
