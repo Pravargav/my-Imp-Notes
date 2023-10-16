b9fefaf7cfe618ab228957385b64f24bcd7f2b7b
Pravargav <96449025+Pravargav@users.noreply.github.com>
2023-10-16 09:43:07 +0530

    Update runJson2.py

--- runJson2.py
+++ runJson2.py
@@ -1,1 +1,15 @@
+from faker import Faker
+import random
 
+fake = Faker()
+
+def generate_paragraph():
+    words = [fake.word() for _ in range(100)]
+
+    paragraph = ' '.join(words)
+    
+    return paragraph
+
+paragraphs = [generate_paragraph() for _ in range(10)]
+
+print(paragraphs)
