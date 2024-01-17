1c6c87a12312e0264dc2543f77a8b94d3654d327
Pravargav <96449025+Pravargav@users.noreply.github.com>
2024-01-17 18:10:48 +0530

    Update deepLearn.py

--- deepLearn.py
+++ deepLearn.py
@@ -13,7 +13,9 @@ from sklearn.model_selection import train_test_split
 
 data = []
 labels = []
-paths= ['agricultural','airplane','baseballdiamond','beach','buildings','chaparral','denseresidential','forest','freeway','golfcourse','harbor','intersection','mediumresidential','mobilehomepark','overpass','parkinglot','river','runway','sparseresidential','storagetanks','tenniscourt']
+paths= ['agricultural','airplane','baseballdiamond','beach','buildings','chaparral','denseresidential',
+        'forest','freeway','golfcourse','harbor','intersection','mediumresidential','mobilehomepark',
+        'overpass','parkinglot','river','runway','sparseresidential','storagetanks','tenniscourt']
 classes = 21
 cur_path = os.getcwd()
 
@@ -49,28 +51,3 @@ X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2,
 
 model = Sequential()
 
-model.add(Conv2D(32, (2, 2), input_shape=(64, 64, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (2, 2)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(21))
-model.add(Activation('softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# Train the model
-model.fit(X_train, y_train, epochs=50, validation_data=(X_test, y_test), batch_size=64)
-
-# Evaluate the model on the test data
-score = model.evaluate(X_test, y_test, batch_size=64)
-
-print("Test Loss:", score[0])
-print("Test Accuracy:", score[1])
