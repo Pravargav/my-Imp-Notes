3914b80145dcee65dd2eef644aae5185446369e0
Pravargav <96449025+Pravargav@users.noreply.github.com>
2024-12-26 19:57:42 +0530

    Create task10.js

---
+++ task10.js
@@ -0,0 +1,8 @@
+public_users.get("/", async (req, res) => {
+  try {
+    const allBooks = await getAllBooks();
+    return res.status(200).send(JSON.stringify(allBooks, null, 4));
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
