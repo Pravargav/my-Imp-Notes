f0ce2495648fd0a638b134e7808549797a6c7b7f
Pravargav <96449025+Pravargav@users.noreply.github.com>
2024-12-28 11:49:26 +0530

    oerogsos
    hhoogjgi

--- task11.js
+++
@@ -1,9 +0,0 @@
-public_users.get("/isbn/:isbn", async (req, res) => {
-  const targetISBN = parseInt(req.params.isbn);
-  const targetBook = await books[targetISBN];
-  if (!targetBook) {
-    return res.status(404).json({ message: "ISBN not found." });
-  } else {
-    return res.status(200).json(targetBook);
-  }
-});
